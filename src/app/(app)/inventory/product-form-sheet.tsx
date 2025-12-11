c7ae77f8e5abf794fa2b89282233d883db8229da
Grim Reaper <grimr3896@gmail.com>
2025-12-11 08:21:18 +0000

    REMOVE THIS SECTION

--- src/app/(app)/inventory/product-form-sheet.tsx
+++ src/app/(app)/inventory/product-form-sheet.tsx
@@ -242,24 +242,22 @@ export function ProductFormSheet({ isOpen, onOpenChange, onSubmit, product }: Pr
                             <Input {...register(`pourVariants.${index}.name`)} placeholder="e.g. 1/4 L" />
                         </div>
                         <div className="col-span-6 sm:col-span-3">
-                           <Controller
+                            <Label>Size (L)</Label>
+                             <Controller
                                 name={`pourVariants.${index}.pourSizeML`}
                                 control={control}
                                 render={({ field: { onChange, value, ...restField } }) => (
-                                    <>
-                                        <Label>Size (L)</Label>
-                                        <Input
-                                            type="number"
-                                            step="0.01"
-                                            value={value > 0 ? value / 1000 : ""}
-                                            onChange={(e) => {
-                                                const valInLiters = parseFloat(e.target.value);
-                                                onChange(isNaN(valInLiters) ? 0 : valInLiters * 1000);
-                                            }}
-                                            {...restField}
-                                            placeholder="e.g. 0.25"
-                                        />
-                                    </>
+                                    <Input
+                                        type="number"
+                                        step="0.01"
+                                        value={value > 0 ? value / 1000 : ""}
+                                        onChange={(e) => {
+                                            const valInLiters = parseFloat(e.target.value);
+                                            onChange(isNaN(valInLiters) ? 0 : valInLiters * 1000);
+                                        }}
+                                        {...restField}
+                                        placeholder="e.g. 0.25"
+                                    />
                                 )}
                             />
                         </div>
